d773a5c7cf85e2bd91eb135d464736ce09007aed
nehsam <nehsam2127@gmail.com>
2025-01-22 16:05:46 +0500

    update

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,13 +1,12 @@
 // @/sanity/lib/client.ts
 import { createClient as sanityClient } from 'next-sanity';
 
-export const createClient = () =>
-  sanityClient({
-    projectId: '15hm7ok6', // Replace with your actual project ID
-    dataset: 'production',
-    token: 'skyEDFMwmS8Y9laviQSbbXOzFxEe1MNwZvgnWURpuUWm5qnn7lmRSltk7tgcClGe4N0m7i8kGAllFLhMg8Ww1A27O49uWYW7EdWLy8Jabhy6Eesk9hGMgvijMDnbsYCIdXajOsrSmxg42pUIXv8YOwkVetGVFv7KqChA4QwWXRCZuV3uaZJr', // Replace with your actual token
-    useCdn: true,
-  });
+export const client = sanityClient({
+  projectId: '15hm7ok6', // Replace with your actual project ID
+  dataset: 'production',
+  token: 'skyEDFMwmS8Y9laviQSbbXOzFxEe1MNwZvgnWURpuUWm5qnn7lmRSltk7tgcClGe4N0m7i8kGAllFLhMg8Ww1A27O49uWYW7EdWLy8Jabhy6Eesk9hGMgvijMDnbsYCIdXajOsrSmxg42pUIXv8YOwkVetGVFv7KqChA4QwWXRCZuV3uaZJr', // Replace with your actual token
+  useCdn: true,
+});
 
 
 
